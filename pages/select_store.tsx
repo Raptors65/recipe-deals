3fe71fb65fafaf779851a776fc12bcb78a8ffb13
Lucas Kim <lucaskamadakim@gmail.com>
2023-05-11 11:39:12 -0400

    Add ability to use current location

--- pages/select_store.tsx
+++ pages/select_store.tsx
@@ -6,6 +6,7 @@ import { getLoblawsStores } from '@/lib/loblaws';
 import LoblawsStoreInfo from '@/components/loblaws_store_info';
 import { FilteredStoreInfo } from '@/types/loblaws';
 import { getNoFrillsStores } from '@/lib/nofrills';
+import getDistance from '@/lib/distance';
 
 type SelectStoreProps = {
   stores: FilteredStoreInfo[]
@@ -16,6 +17,26 @@ export default function SelectStore({ stores }: SelectStoreProps) {
 
   const [query, setQuery] = useState('');
   const [hasSearched, setHasSearched] = useState(false);
+  const [latitude, setLatitude] = useState<number | null>(null);
+  const [longitude, setLongitude] = useState<number | null>(null);
+  const [error, setError] = useState<string | null>(null);
+
+  const sort = (a: FilteredStoreInfo, b: FilteredStoreInfo) => {
+    if (latitude === null || longitude === null) return 0;
+    const distanceA = getDistance(
+      a.geoPoint.latitude,
+      a.geoPoint.longitude,
+      latitude,
+      longitude,
+    );
+    const distanceB = getDistance(
+      b.geoPoint.latitude,
+      b.geoPoint.longitude,
+      latitude,
+      longitude,
+    );
+    return distanceA - distanceB;
+  };
 
   if (!hasSearched && typeof router.query.chain === 'string' && router.query.chain !== query) {
     setQuery(router.query.chain);
@@ -27,7 +48,7 @@ export default function SelectStore({ stores }: SelectStoreProps) {
         <title>Select Loblaws - Recipe Deals</title>
       </Head>
       <input
-        className="border-2"
+        className="border-2 p-1"
         placeholder="Search store..."
         type="text"
         value={query}
@@ -36,10 +57,29 @@ export default function SelectStore({ stores }: SelectStoreProps) {
           if (!hasSearched) setHasSearched(true);
         }}
       />
+      <button
+        className="border-2 bg-green-200 hover:bg-green-400 p-1"
+        onClick={() => {
+          if (!('geolocation' in navigator)) {
+            setError('Location not supported by your browser');
+            return;
+          }
+          navigator.geolocation.getCurrentPosition((position) => {
+            setLatitude(position.coords.latitude);
+            setLongitude(position.coords.longitude);
+            setError(null);
+          }, () => setError('Unable to retrieve your location'));
+        }}
+        type="button"
+      >
+        Use Current Location
+      </button>
+      {error !== null && <p className="text-red-600">{error}</p>}
+
       <div className="w-full flex-wrap">
         {stores.filter((store) => ((((store.storeBannerId === 'loblaw' && 'Loblaws - ') + store.name).toLowerCase().includes(query.toLowerCase())
                                   || store.address.toLowerCase().includes(query.toLowerCase()))))
-          .slice(0, 10).map((store) => (
+          .sort(sort).slice(0, 10).map((store) => (
             <LoblawsStoreInfo key={store.id} store={store} />
           ))}
       </div>
